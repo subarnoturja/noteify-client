6fe1d7eff4514b52a132eabb621361959d49ff8e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-21 14:55:06 +0600

    name and logout added in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-
+import { Link, useNavigate } from "react-router-dom";
+import ProfileInfo from "./Cards/ProfileInfo";
 
 const Navbar = () => {
-    return (
-        <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-           <h2 className="text-xl font-medium text-black py-2">Noteify</h2> 
-        </div>
-    );
+
+    const navigate = useNavigate();
+
+   const onLogout = () => {
+    navigate('/login');
+   }
+
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <Link to='/'>
+        <h2 className="text-xl font-medium text-black py-2">Noteify</h2>
+      </Link>
+      <ProfileInfo onLogout={onLogout}></ProfileInfo>
+    </div>
+  );
 };
 
 export default Navbar;
