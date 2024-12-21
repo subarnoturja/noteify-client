509fe78ffd5763cf73965d70d6279a37200a2913
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-21 15:54:38 +0600

    basic note card added

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
-
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet> 
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="container mx-auto">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
-
+import { MdAdd } from "react-icons/md";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
-    return (
-        <div>
-            <h1 className="text-center text-2xl font-bold">This is Home Page</h1>
-        </div>
-    );
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4 mt-8">
+        <NoteCard
+          title="Meeting on 7th April"
+          date="3rd April"
+          content="Meeting on 7th April Meeting on 7th April"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        ></NoteCard>
+      </div>
+      <button className="w-16 h-16 btn flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <MdAdd className="text-[32px] text-white"></MdAdd>
+      </button>
+    </>
+  );
 };
 
 export default Home;
