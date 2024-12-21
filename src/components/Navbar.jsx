f4cd7d71a9f669323fe5773c560f81259e6ed9cf
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-21 15:14:28 +0600

    search bar added in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
 import { Link, useNavigate } from "react-router-dom";
 import ProfileInfo from "./Cards/ProfileInfo";
+import SearchBar from "./SearchBar/SearchBar";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   const onLogout = () => {
-    navigate('/login');
-   }
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {
+
+  }
+
+  const onClearSearch = () => {
+    setSearchQuery("")
+  }
 
   return (
-    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <Link to='/'>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow mb-6">
+      <Link to="/">
         <h2 className="text-xl font-medium text-black py-2">Noteify</h2>
       </Link>
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      ></SearchBar>
       <ProfileInfo onLogout={onLogout}></ProfileInfo>
     </div>
   );
